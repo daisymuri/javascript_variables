2c0e428761ada640d5e9d538ccfc5cb3c3bc31d3
daisymuri <daisy21murillo@gmail.com>
2022-08-03 16:42:25 -0700

    adding variables

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+// let, const, and var
+
+// let variable
+let name = "John Doe";
+
+// const variable
+const fruit = "Apple";
+
+// var variable
+var x = 10;
+
+//Data Types - dynamic types, but typically use: String, Numbers, or Objects
+let a; 
+a = 5;
+a = "John";
